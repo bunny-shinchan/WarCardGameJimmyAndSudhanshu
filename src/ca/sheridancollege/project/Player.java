27df1faf3bef45d7730154c014cf2241e6458c64
vermsudh <verms@LAPTOP-8JUD22D0>
2020-06-28 22:32:56 -0400

    This is the code of Jimmy and Sudhanshu

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -1,13 +1,14 @@
 /**
  * SYST 17796 Project Winter 2019 Base code.
- * Students can modify and extend to implement their game.
- * Add your name as a modifier and the date!
+ * 
+ * Name of the coders: Sudhanshu and Jimmy
+ * Date of the code : th june 2020
  */
 package ca.sheridancollege.project;
 
 /**
  * A class that models each Player in the game. Players have an identifier, which should be unique.
- * @author dancye, 2018
+ *
  */
 public abstract class Player 
 {
